1bf0c52d549f5e27ef986af2c2223b7f96d8fece
Maciej Stefańczyk <maciek.slon@gmail.com>
2021-07-26 17:55:31 +0200

    Add stereo flag for image_saver

--- nodes/image_saver.py
+++ nodes/image_saver.py
@@ -14,22 +14,34 @@ import os
 
 
 class ConfigSaver:
-    def __init__(self, filename):
+    def __init__(self):
         self.trigger = False
         self.counter = 0
 
     def triggerCb(self, data):
         self.trigger = True
+        print("Trigger")
 
-    def gotimage(self, image):
+    def saveImage(self, image, name):
+        cv_image = self.bridge.imgmsg_to_cv2(image, desired_encoding='passthrough')
+        fname = name + '_' + str(image.header.stamp.secs) + '_' + str(image.header.stamp.nsecs) + ".jpg"
+        cv2.imwrite(fname, cv_image)
+ 
+
+    def imageCb(self, image):
         if self.trigger:
             self.trigger = False
-            cv_image = self.bridge.imgmsg_to_cv2(image, desired_encoding='passthrough')
-            fname = 'img_' + str(image.stamp.secs) + '_' + str(image.stamp.nsecs) + ".jpg"
-            cv2.imwrite(fname, cv_image)
+            self.saveImage(image, 'img')
             self.counter += 1
             self.pub.publish(self.counter)
 
+    def stereoCb(self, left, right):
+        if self.trigger:
+            self.trigger = False
+            self.saveImage(left, 'left')
+            self.saveImage(right, 'right')
+            self.counter += 1
+            self.pub.publish(self.counter)
 
     def spin(self):
         # In ROS, nodes are uniquely named. If two nodes with the same
@@ -40,16 +52,26 @@ class ConfigSaver:
         rospy.init_node('image_saver', anonymous=True)
 
         rospy.sleep(0.5)
+	self.stereo = rospy.get_param('~stereo', False)
+
         self.bridge = CvBridge()
 
         rospy.Subscriber("trigger", String, self.triggerCb)
-        self.pub = rospy.Publisher("saved_positions", Int32)
-        image_sub = Subscriber("/camera/image", Image)
+        self.pub = rospy.Publisher("saved_images", Int32, queue_size = 10)
+
+        if self.stereo:
+            left_sub = Subscriber("left", Image)
+            right_sub = Subscriber("right", Image)
+
+            ats = ApproximateTimeSynchronizer([left_sub, right_sub], queue_size=5, slop=0.01)
+            ats.registerCallback(self.stereoCb)
+        else:
+            rospy.Subscriber("image", Image, self.imageCb)
 
         rospy.spin()
 
 def main():
-    cs = ConfigSaver(sys.argv[1])
+    cs = ConfigSaver()
     cs.spin()
 
 if __name__ == '__main__':
